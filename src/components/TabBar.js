e77f8309a715a5c90fd5945c3836ecba5b481211
Brad Cornes <hello@bradley.dev>
2022-03-30 16:26:23 +0100

    Only show "tidy" button for monaco editor

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,4 +1,6 @@
 import clsx from 'clsx'
+import { useEffect, useState } from 'react'
+import isMobile from 'is-mobile'
 
 export function TabBar({
   activeTab,
@@ -8,6 +10,14 @@ export function TabBar({
   onChange,
   onTidy,
 }) {
+  let [isTidyable, setIsTidyable] = useState(false)
+
+  useEffect(() => {
+    if (!isMobile()) {
+      setIsTidyable(true)
+    }
+  }, [])
+
   return (
     <div
       className="flex items-center flex-none pl-5 sm:pl-6 pr-4 lg:pr-6 absolute z-10 top-0 left-0 -mb-px antialiased"
@@ -46,7 +56,7 @@ export function TabBar({
       </div>
       <div className="ml-auto flex items-center">
         {isLoading && (
-          <p className="mr-4">
+          <p>
             <span className="sr-only">Loading</span>
             <svg
               fill="none"
@@ -69,28 +79,30 @@ export function TabBar({
             </svg>
           </p>
         )}
-        <button
-          type="button"
-          className="text-sm font-semibold text-gray-500 flex items-center hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-          onClick={onTidy}
-        >
-          <svg
-            viewBox="0 0 24 24"
-            className="w-6 h-6 mr-1"
-            fill="currentColor"
-            aria-hidden="true"
+        {isTidyable && (
+          <button
+            type="button"
+            className="ml-4 text-sm font-semibold text-gray-500 flex items-center hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+            onClick={onTidy}
           >
-            <path
-              d="M5 9a2 2 0 0 1 2 2 1 1 0 1 0 2 0 2 2 0 0 1 2-2 1 1 0 1 0 0-2 2 2 0 0 1-2-2 1 1 0 0 0-2 0 2 2 0 0 1-2 2 1 1 0 0 0 0 2Z"
-              className="text-gray-400 dark:text-gray-500"
-            />
-            <path
-              d="M11 16a3 3 0 0 1 3 3 1 1 0 1 0 2 0 3 3 0 0 1 3-3 1 1 0 1 0 0-2 3 3 0 0 1-3-3 1 1 0 1 0-2 0 3 3 0 0 1-3 3 1 1 0 1 0 0 2Z"
-              className="text-gray-300 dark:text-gray-400"
-            />
-          </svg>
-          Tidy<span className="sr-only">, and focus editor</span>
-        </button>
+            <svg
+              viewBox="0 0 24 24"
+              className="w-6 h-6 mr-1"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                d="M5 9a2 2 0 0 1 2 2 1 1 0 1 0 2 0 2 2 0 0 1 2-2 1 1 0 1 0 0-2 2 2 0 0 1-2-2 1 1 0 0 0-2 0 2 2 0 0 1-2 2 1 1 0 0 0 0 2Z"
+                className="text-gray-400 dark:text-gray-500"
+              />
+              <path
+                d="M11 16a3 3 0 0 1 3 3 1 1 0 1 0 2 0 3 3 0 0 1 3-3 1 1 0 1 0 0-2 3 3 0 0 1-3-3 1 1 0 1 0-2 0 3 3 0 0 1-3 3 1 1 0 1 0 0 2Z"
+                className="text-gray-300 dark:text-gray-400"
+              />
+            </svg>
+            Tidy<span className="sr-only">, and focus editor</span>
+          </button>
+        )}
       </div>
     </div>
   )
