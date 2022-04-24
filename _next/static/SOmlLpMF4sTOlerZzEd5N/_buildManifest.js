fd5584c3a9606cbce2d23ba0b1c7171409c04aee
dtinth <dtinth@users.noreply.github.com>
2022-04-24 01:39:05 +0000

    deploy: 75bf2703a6d23d8f15f33cea38ca1e812308c4e1

--- _next/static/SOmlLpMF4sTOlerZzEd5N/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/4299-a003e87f846b14ea.js","static/chunks/pages/index-e639e03d3a9b1c9b.js"],"/_error":["static/chunks/pages/_error-25839e52160ad85d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
