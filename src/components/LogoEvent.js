51c8818d4007540d5f707a54bcbc1add8a050913
dtinth on MBP M1 <dtinth@spacet.me>
2022-04-26 22:40:31 +0700

    Bolden logo

--- src/components/LogoEvent.js
+++ src/components/LogoEvent.js
@@ -1,3 +1,7 @@
 export function Logo({ className }) {
-  return <div style={{ width: 171, height: 21 }}>Code in the Wind</div>
+  return (
+    <div style={{ width: 171, height: 21 }} className="font-bold">
+      Code in the Wind
+    </div>
+  )
 }
