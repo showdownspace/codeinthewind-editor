ef087b560cafb800defff53ab5b9ddc36df832b6
dtinth <dtinth@users.noreply.github.com>
2022-05-13 18:27:41 +0000

    deploy: 1be53af3593ede9598a5e522f4794c179268cd79

--- _next/static/mush7Qdy8lvpjEH2hTwtK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7862-dc2dba430e91853a.js","static/chunks/pages/index-7ae8625f965555b4.js"],"/_error":["static/chunks/pages/_error-25839e52160ad85d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/wt1eVdHvaGJTIXavxY5qp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2355-7611e853ef7d8e65.js","static/chunks/pages/index-e9b7e7155d75449d.js"],"/_error":["static/chunks/pages/_error-25839e52160ad85d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
